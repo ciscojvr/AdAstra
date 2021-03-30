ab0f7b485615543c46bb5360fd3c2bb559be89a6
Francisco Ozuna <ozuna252@hotmail.com>
2021-03-30 19:51:04 -0400

    Created IBOutlets for labels in UI that will change based on JSON response from API call. Created IBAction to handle when user presses on the yesterday, today or tomorrow buttons. These buttons will display the Astronomy photo of the day for those respective days.

--- AdAstra/Controllers/ViewController.swift
+++ AdAstra/Controllers/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageTitle: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var imageDescription: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        switch sender.titleLabel?.text {
+        case "Yesterday":
+            print("Yesterday Button Pressed.")
+        case "Today":
+            print("Today Button Pressed.")
+        case "Tomorrow":
+            print("Tomorrow Button Pressed.")
+        default:
+            print("Unknown Button Pressed.")
+        }
+    }
 }
 
