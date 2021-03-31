c2c64f3290fd0269cdeb9c7ab905b9e5010e5099
Francisco Ozuna <ozuna252@hotmail.com>
2021-03-30 22:01:00 -0400

    Create ImageManager to facilitate process of obtaining photo of the day from APOD API. Set the current view controller as delegate of this image manager so it can grab todays photo of the day, or yesterdays, based on user pressing the button. Created extension to house ImageManagerDelegate code for better readability. Created extension to UIImageView to load an image from the url string provided by the APOD API. Removed tomorrow button as the API does support this functionality.

--- AdAstra/Controllers/ViewController.swift
+++ AdAstra/Controllers/ViewController.swift
@@ -9,25 +9,67 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var today: String = ""
+    var yesterday: String = ""
+    
     @IBOutlet weak var imageTitle: UILabel!
     @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var imageDescription: UILabel!
+    @IBOutlet weak var imageDescription: UITextView!
+    
+    var imageManager = ImageManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        imageManager.delegate = self
+        
+        let todaysDate = Date()
+        let yesterdaysDate = Calendar.current.date(byAdding: .day, value: -1, to: Date())!
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyy-MM-dd"
+        today = formatter.string(from: todaysDate)
+        yesterday = formatter.string(from: yesterdaysDate)
+        
+        imageManager.getPhoto(for: today)
     }
     
     @IBAction func buttonPressed(_ sender: UIButton) {
         switch sender.titleLabel?.text {
         case "Yesterday":
-            print("Yesterday Button Pressed.")
+            imageManager.getPhoto(for: yesterday)
         case "Today":
-            print("Today Button Pressed.")
-        case "Tomorrow":
-            print("Tomorrow Button Pressed.")
+            imageManager.getPhoto(for: today)
         default:
-            print("Unknown Button Pressed.")
+            return
+        }
+    }
+}
+
+extension ViewController: ImageManagerDelegate {
+    func didUpdateImage(_ imageManager: ImageManager, image: ImageModel) {
+        DispatchQueue.main.async {
+            self.imageTitle.text = " \"\(image.photoTitle)\" "
+            self.imageView.load(url: URL(string: image.photoUrl)!)
+            self.imageDescription.text = image.photoDescription
+        }
+    }
+    
+    func didFailWithError(error: Error) {
+        print(error)
+    }
+}
+
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
         }
     }
 }
