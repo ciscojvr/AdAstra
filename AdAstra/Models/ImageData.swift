f4771e749b6fff64f8376382b679bee8cece07bc
Francisco Ozuna <ozuna252@hotmail.com>
2021-03-30 22:03:32 -0400

    Created ImageData struct which conforms to the Codable (Encodable & Decodable) protocol. This is used to provide our ImageManager with the structure with which to encode the JSON data returned from our API call.

---
+++ AdAstra/Models/ImageData.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoData.swift
+//  AdAstra
+//
+//  Created by Francisco Ozuna on 3/30/21.
+//
+
+import Foundation
+
+struct ImageData: Codable {
+    let title: String
+    let url: String
+    let explanation: String
+}
